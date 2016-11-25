3fc9fac6220794d1cc16408672a48d1d165d12d8
102213029 <acc95050423@gmail.com>
2016-11-25 20:50:22 +0800

    update

--- ajaxver/chooseCate.php
+++ ajaxver/chooseCate.php
@@ -85,13 +85,19 @@ $.ajax({
 </script>
 <title>TEA Question</title>
 </head>
-
 <body>
-<input type="button" onclick="C1(1)" value="臺灣茶樹品種與特色茶認識">
+<div id="catemenu">
+<a href="javascript:C1(1)"><div id="sidenavbt3" class="hvr-shrink">臺灣茶樹品種與特色茶認識</div></a>
+<a href="javascript:C2(2)"><div id="sidenavbt3" class="hvr-shrink">茶葉製造</div></a>
+<a href="javascript:C3(3)"><div id="sidenavbt3" class="hvr-shrink">茶葉品質管理</div></a>
+<a href="javascript:C4(4)"><div id="sidenavbt3" class="hvr-shrink">食品衛生與工業安全</div></a>
+<a href="javascript:C5(5)"><div id="sidenavbt3" class="hvr-shrink">製茶職業道德</div></a>
+</div>
+<!--<input type="button" onclick="C1(1)" value="臺灣茶樹品種與特色茶認識">
 <input type="button" onclick="C2(2)" value="茶葉製造">
 <input type="button" onclick="C3(3)" value="茶葉品質管理">
 <input type="button" onclick="C4(4)" value="食品衛生與工業安全">
-<input type="button" onclick="C5(5)" value="製茶職業道德">
+<input type="button" onclick="C5(5)" value="製茶職業道德">-->
 <br/>
 <hr/>
 </body>
